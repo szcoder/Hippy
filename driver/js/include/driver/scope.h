81eb1b389524a050c3f4ec365d45598f5e98fe23
sohotz <sohotz@qq.com>
2025-01-08 20:26:38 +0800

    feat(ohos): fix jsvm gc

--- driver/js/include/driver/scope.h
+++ driver/js/include/driver/scope.h
@@ -364,13 +364,19 @@ class Scope : public std::enable_shared_from_this<Scope> {
         return;
       }
       info.SetData(ret.get());
+#ifdef __OHOS__
+      context->SetReceiverData(receiver, ret.get());
+#endif
+      // FOOTSTONE_DLOG(INFO) << "hippy gc, holder map insert, class_tp: " << class_template << ", " << class_template->name << ", obj: " << ret.get();
       class_template->holder_map.insert({ret.get(), ret});
       FOOTSTONE_CHECK(context);
       auto weak_callback_wrapper = std::make_unique<WeakCallbackWrapper>([](void* callback_data, void* internal_data) {
         auto class_template = reinterpret_cast<ClassTemplate<T>*>(callback_data);
         auto& holder_map = class_template->holder_map;
+        // FOOTSTONE_DLOG(INFO) << "hippy gc, holder map erase, class_tp: " << class_template << ", " << class_template->name << ", obj: " << internal_data;
         auto it = holder_map.find(internal_data);
         if (it != holder_map.end()) {
+          // FOOTSTONE_DLOG(INFO) << "hippy gc, holder map erase success";
           holder_map.erase(it);
         }
       }, class_template);
